247d2a9530755884853a3d09c64ce25309f56524
lavaman131 <lavaalex3@gmail.com>
2024-05-18 13:21:08 -0400

    added data prep code for rat7m

--- lib/utils/prep_rat7m.py
+++ lib/utils/prep_rat7m.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 from collections import defaultdict
 import pickle as pkl
+from tqdm.auto import tqdm
 
 s_36_flip_pairs = np.array(
     [[1, 4], [2, 5], [3, 6], [14, 11], [15, 12], [16, 13]], dtype=np.int32
@@ -57,7 +58,7 @@ def get_bbox_info(
 
 # Notes: inside each Camera1, ... dirs there are camera_params.json and images_every20
 def create_dataset(
-    subject_ids: List[str],  # e.g. ["s1-d1", ...]
+    subject_fnames: List[str],  # e.g. ["s1-d1", ...]
     split: str,  # e.g. "train"
     data_dir: Union[
         str, Path
@@ -71,18 +72,20 @@ def create_dataset(
     save_dir = Path(save_dir) if isinstance(save_dir, str) else save_dir
 
     num_joints = 20
-    joints_3d_all = np.load(
-        data_dir.joinpath("mocap_20.npy")
-    )  # shape: (num_samples, num_joints * 3)
-    joints_3d_all = joints_3d_all.reshape(
-        -1, num_joints, 3
-    )  # shape: (num_samples, num_joints, 3)
     # Note: contains NaNs
     data = defaultdict(list)
     camera_dir_names = [f"Camera{i}" for i in range(1, 7)]
-    for subject_id in subject_ids:
+    for subject_fname in tqdm(subject_fnames):
+        subject_id = subject_fname.split("-")[0]
+        subject_dir = data_dir.joinpath(subject_fname)
+        joints_3d_all = np.load(
+            subject_dir.joinpath("mocap_20.npy")
+        )  # shape: (num_samples, num_joints * 3)
+        joints_3d_all = joints_3d_all.reshape(
+            -1, num_joints, 3
+        )  # shape: (num_samples, num_joints, 3)
         for camera_idx, camera_dir_name in enumerate(camera_dir_names):
-            camera_dir = data_dir.joinpath(camera_dir_name)
+            camera_dir = subject_dir.joinpath(camera_dir_name)
             with open(camera_dir.joinpath("camera_params.json")) as f:
                 camera_params = json.load(f)
             R, T, K, f, c, projection_matrix = extract_camera_params(camera_params)
@@ -93,6 +96,8 @@ def create_dataset(
                 image_relative_path = image_path.relative_to(data_dir)
                 idx_3d = int(image_relative_path.name.split("_")[0])
                 joints_3d = joints_3d_all[idx_3d]
+                if np.isnan(joints_3d).all():
+                    continue
                 joints_3d_vis = np.ones_like(joints_3d)
                 action = ""
                 center_x, center_y, width, height = get_bbox_info(joints_3d, R, T, K)
@@ -111,7 +116,16 @@ def create_dataset(
                     "action": action,
                 }
                 data[camera_id].append(sample)
-
-    save_dir.mkdir(parents=True, exist_ok=True)
-    with open(save_dir.joinpath("annot", f"{split}.pkl"), "wb") as f:
+    annot_save_dir = save_dir.joinpath("annot")
+    annot_save_dir.mkdir(parents=True, exist_ok=True)
+    with open(annot_save_dir.joinpath(f"{split}.pkl"), "wb") as f:
         pkl.dump(data, f)
+
+
+if __name__ == "__main__":
+    data_dir = "/projectnb/ivc-ml/Datasets/PoseEstimation/Rat7M"
+    save_dir = "/projectnb/ivc-ml/alavaee/data/Rat7M_processed"
+    train_subject_fnames = ["s1-d1", "s2-d1", "s2-d2", "s3-d1", "s4-d1"]
+    test_subject_fnames = ["s5-d1", "s5-d2"]
+    create_dataset(train_subject_fnames, "train", data_dir, save_dir)
+    create_dataset(test_subject_fnames, "test", data_dir, save_dir)
